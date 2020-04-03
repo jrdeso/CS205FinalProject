5196a136b48c0888983da2d54ac0ee7cdc53591a
Edmund Do <edo@uvm.edu>
2020-04-03 04:03:48 -0400

    sequence buffer data structure

---
+++ towerd/test/test_buffer.py
@@ -0,0 +1,30 @@
+import unittest
+
+from ..util.buffer import SequenceBuffer
+
+
+class TestResources(unittest.TestCase):
+    def setUp(self):
+        self.max_length = 32
+        self.seq_buffer = SequenceBuffer(self.max_length)
+
+        self.man_idx = 10
+        self.man_val = 12
+        self.seq_buffer.buf[self.man_idx] = self.man_val
+
+    def test_setter_getter(self):
+        i = 38
+        val = 23
+
+        self.seq_buffer[i] = val
+        self.assertEqual(self.seq_buffer[i], val)
+
+    def test_len(self):
+        self.assertEqual(self.max_length, len(self.seq_buffer))
+
+    def test_contains(self):
+        self.assertTrue(0 in self.seq_buffer)
+        self.assertFalse(32 in self.seq_buffer)
+
+    def test_index(self):
+        self.assertEqual(self.man_idx, self.seq_buffer.index(self.man_val))
---
+++ towerd/util/buffer.py
@@ -0,0 +1,25 @@
+class SequenceBuffer:
+    def __init__(self, max_length):
+        self.buf = list((0,) * max_length)
+        self.dat = list((None,) * max_length)
+        self.max_length = max_length
+
+        self.index = self.buf.index
+
+    def __getitem__(self, seq):
+        i = seq % self.max_length
+        return self.dat[i] if self.buf[i] == seq else None
+
+    def __setitem__(self, seq, val):
+        i = seq % self.max_length
+        self.buf[i] = seq
+        self.dat[i] = val
+
+    def __len__(self):
+        return self.max_length
+
+    def __contains__(self, item):
+        return item in self.buf
+
+    def __iter__(self):
+        return iter(self.buf)
