60ad0b13e4bf7056d8f4fad80d1e7ae86447a907
Carlos Castrejon <carlitoscs99@gmail.com>
2020-04-09 11:37:25 +0200

    Started mob and game class

---
+++ towerd/Game.py
@@ -0,0 +1,37 @@
+import os
+import pygame
+
+class Game:
+	def __init__(self):
+		self.width = 1200 #Width of the pygame window
+		self.height = 700 #Height of the pygame window
+		self.win = pygame.display.set_mode((self.width, self.height))
+		self.enemies = []
+		self.towers = []
+		self.player_lives = 20
+		self.player_money = 1000
+		self.background = pygame.image.load(os.path.join("xxxxxxx", "xxxxx.png"))
+		self.background = pygame.transform.scale(self.background, (self.width, self.height))
+
+
+
+	def run(self): #This is the run method to control all the dynamics of the game
+		run = True
+		clock = pygame.time.Clock() #To set the FPS of the game
+		while run:
+			clock.tick(60) #This runs the fame at 60 FPS
+
+
+			# main event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+			pygame.display.update()
+
+
+	#def draw(self):
+	#	self.win.blit(self.background, (0, 0))
+	#	pygame.display.update()
+
+
--- towerd/Mob.py
+++ towerd/Mob.py
@@ -1,4 +1,30 @@
+import pygame
 
 class Mob:
-    def __init__(self):
-        pass
+
+	sprites = []
+
+    def __init__(self, x, y):
+        self.x = x #This represents the coordinates x of the mob
+        self.y = y #This represents the coordinates y of the mob
+        self.animation_count = 0 #This variable is used to animate the mobs (We could do them without animating them). It starts at 0 which is the default position for going straight
+        self.health = 100 #Base health for basic mobs is 100. We have to adjust this number
+        self.sprite =  None
+
+
+    def draw(self, win): #This method will draw the emobs
+    	win.blit() #Fill this with apropiate information
+
+
+   	def move(self): #This method control the movements of the mobs
+   		self.animation_count = self.animation_count + 1
+   		if self.animation_count >= len(self.sprites):
+   			self.animation_count = 0 #This resets the animations back
+
+
+   	def killed(self, dmg): #Method for when an enemy is killed by a tower
+   		self.health = self.health - dmg
+   		if self.health <= 0:
+   			return True
+   		return False
+
