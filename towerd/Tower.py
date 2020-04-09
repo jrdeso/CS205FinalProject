70d368e7ce63a43f5fcc81d03fa1bcd52a84d4cc
patrickniccolai <patrick.niccolai@uvm.edu>
2020-04-09 10:46:50 -0400

    Removed health, added upgrade and coordinates

--- towerd/Tower.py
+++ towerd/Tower.py
@@ -1,26 +1,28 @@
 from enum import Enum
+
+# We might need to move this enum to the game class
 class TowerSubclass(Enum):
     Soldier = 1
     Archer = 2
     Mage = 3
+
 class Tower:
     # Constructor
-    def __init__(self, subclass):
+    def __init__(self, subclass, x, y):
+        self.xCoord = x
+        self.yCoord = y
         if(subclass == Soldier):
             self.subclass = Soldier
-            self.health = 100
             self.damage = 10
             self.range = 2
             self.level = 0
         if(subclass == Archer):
             self.subclass = Archer
-            self.health = 75
             self.damage = 7
             self.range = 5
             self.level = 0
         if(subclass == Mage):
             self.subclass = Mage
-            self.health = 125
             self.damage = 10
             self.range = 4
             self.level = 0
@@ -28,9 +30,6 @@ class Tower:
     # Getters
     def getSubclass(self):
         return self.subclass
-
-    def getHealth(self):
-        return self.health
 
     def getDamage(self):
         return self.damage
@@ -40,3 +39,33 @@ class Tower:
 
     def getLevel(self):
         return self.level
+
+    # Upgrade
+    def upgrade(self):
+        if(self.level <= 2):
+            self.level+= 1
+            self.damage+=3
+
+    # Get cost for upgrading, these numbers can be changed later depending on how much money the player gets per enemy/wave
+    def getUpgradeCost(self):
+        if(self.subclass == Soldier):
+            if(self.level == 0):
+                return 100
+            elif(self.level == 1):
+                return 150
+            else:
+                return 200
+        elif(self.subclass == Archer):
+            if (self.level == 0):
+                return 150
+            elif (self.level == 1):
+                return 200
+            else:
+                return 250
+        else:
+            if (self.level == 0):
+                return 200
+            elif (self.level == 1):
+                return 250
+            else:
+                return 300
