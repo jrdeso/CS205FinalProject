06e4d34c6104c29fa3d03a2de7326ead7f66a527
Edmund Do <edo@uvm.edu>
2020-04-10 14:51:41 -0400

    added a map generation test case

--- towerd/test/test_map.py
+++ towerd/test/test_map.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from ..Map import Map, MapNode, PathType
@@ -14,4 +15,17 @@ class TestMap(unittest.TestCase):
         m.addEdge(n2, n3)
 
         self.assertTrue(m.existsEdge(n1, n3))
+        self.assertFalse(m.existsEdge(n3, n1))
         self.assertFalse(m.existsEdge(n1, n2))
+
+    def test_generate(self):
+        jsonstr = '{"0":{"type":"path_start","coord":[0.0,0.05],"edges":["1"]},"1":{"type":"path_end","coord":[0.1,0.15],"edges":[]}}'
+        map_json = json.loads(jsonstr)
+
+        m = Map(map_json=map_json)
+        self.assertTrue(m.existsEdge(0, 1))
+        self.assertFalse(m.existsEdge(1, 0))
+
+        self.assertEqual(m.nodes[0].pathType, PathType.PATH_START)
+        self.assertEqual(m.nodes[0].x, 0.0)
+        self.assertEqual(m.nodes[0].y, 0.05)
