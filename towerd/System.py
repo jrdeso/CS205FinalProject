25b4f322580460b7e63df376ea5c655d10ca3463
Edmund Do <edo@uvm.edu>
2020-04-10 19:43:58 -0400

    added MovementSystem

--- towerd/System.py
+++ towerd/System.py
@@ -11,6 +11,32 @@ class System(abc.ABC):
         raise NotImplementedError
 
 
+class MovementSystem(System):
+    def update(self, dt, state, movement_comps, loc_comps):
+        for entity in self.entities:
+            movement_comp = movement_comps[entity.id]
+            speed = movement_comp.movement_speed
+            from_node = movement_comp.from_node
+            dest_node = movement_comp.dest_node
+
+            m = state['map']
+            from_node = m.node[from_node]
+            dest_node = m.node[dest_node]
+            loc_comp = loc_comps[entity.id]
+
+            from_x, from_y = from_node.x, from_node.y
+            dest_x, dest_y = dest_node.x, dest_node.y
+
+            total_diff_x = dest_x - from_x
+            total_diff_y = dest_y - from_y
+
+            theta = math.atan(total_diff_y / total_diff_x)
+            dl = speed * dt
+
+            loc_comp.x += dl * math.cos(theta)
+            loc_comp.y += dl * math.sin(theta)
+
+
 class SystemManager:
     def __init__(self):
         self.systems = {}
