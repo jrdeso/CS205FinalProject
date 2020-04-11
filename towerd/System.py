0ed8673abf4e791b44e555782557e38eab79b201
Edmund Do <edo@uvm.edu>
2020-04-10 21:14:12 -0400

    update the coordinates

--- towerd/System.py
+++ towerd/System.py
@@ -7,34 +7,48 @@ class System(abc.ABC):
         self.entities = set()
 
     @abc.abstractmethod
-    def update(self, state, *args, **kwargs):
+    def update(self, dt, state, *args, **kwargs):
         raise NotImplementedError
 
 
 class MovementSystem(System):
     def update(self, dt, state, movement_comps, loc_comps):
         for entity in self.entities:
-            movement_comp = movement_comps[entity.id]
+            movement_comp = movement_comps[entity.e_id]
             speed = movement_comp.movement_speed
             from_node = movement_comp.from_node
             dest_node = movement_comp.dest_node
 
             m = state['map']
-            from_node = m.node[from_node]
-            dest_node = m.node[dest_node]
-            loc_comp = loc_comps[entity.id]
+            from_node = m.nodes[from_node]
+            dest_node = m.nodes[dest_node]
+            loc_comp = loc_comps[entity.e_id]
 
-            from_x, from_y = from_node.x, from_node.y
+            from_x, from_y = loc_comp.x, loc_comp.y
             dest_x, dest_y = dest_node.x, dest_node.y
 
             total_diff_x = dest_x - from_x
             total_diff_y = dest_y - from_y
 
-            theta = math.atan(total_diff_y / total_diff_x)
+            theta = math.atan2(total_diff_y, total_diff_x)
             dl = speed * dt
 
-            loc_comp.x += dl * math.cos(theta)
-            loc_comp.y += dl * math.sin(theta)
+            dx = dl * math.cos(theta)
+            dy = dl * math.sin(theta)
+
+            if from_x < dest_x and (new_x := from_x + dx) < dest_x:
+                loc_comp.x = new_x
+            elif from_x > dest_x and (new_x := from_x - dx) > dest_x:
+                loc_comp.x = new_x
+            else:
+                loc_comp.x = dest_x
+
+            if from_y < dest_y and (new_y := from_y + dy) < dest_y:
+                loc_comp.y = new_y
+            elif from_y > dest_y and (new_y := from_y - dy) > dest_y:
+                loc_comp.y = new_y
+            else:
+                loc_comp.y = dest_y
 
 
 class SystemManager:
@@ -53,7 +67,6 @@ class SystemManager:
     def update_system_entity(self, entity, entity_bitset):
         for system in self.systems.values():
             system_bits = self.system_bits[system.__class__.__name__]
-            print(bin(system_bits), bin(entity_bitset))
             if system_bits & entity_bitset == system_bits:
                 system.entities.add(entity)
             else:
