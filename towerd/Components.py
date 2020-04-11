4e11eeda9087ad5623931c8469c0aaabf3a02aa6
Edmund Do <edo@uvm.edu>
2020-04-11 13:35:22 -0400

    documentation

--- towerd/Components.py
+++ towerd/Components.py
@@ -5,16 +5,38 @@ from towerd.util.packed_array import MappedPackedArray
 
 
 class ComponentManager:
+    """
+    Manages all instances of registered components.
+
+    :param max_entities: the maximum number of entities in the game
+
+    :var components: a dictionary mapping the component name to its unique bitset
+    :var component_arrs: an list of packed arrays of components
+    :var max_entities: store of the max_entities parameter
+    """
     def __init__(self, max_entities):
         self.components = {}
         self.component_arrs = []
         self.max_entities = max_entities
 
     def register(self, T):
+        """
+        Register a Component. Generates a unique bitset and adds it to the
+        dictionary of components and associates a packed array with the
+        component.
+
+        :param T: the component class
+        """
         self.components[T.__name__] = len(self.component_arrs)
         self.component_arrs.append(MappedPackedArray(self.max_entities))
 
     def get_component_bits(self, *Ts):
+        """
+        Get a bitset uniquely identifying the component class that are
+        specified.
+
+        :param Ts: any number of Component arguments
+        """
         component_bits = 0
         for T in Ts:
             bitshift = self.components[T.__name__]
@@ -22,17 +44,39 @@ class ComponentManager:
         return component_bits
 
     def get_component_arr(self, T):
+        """
+        Get a packed array of components associated with an entity.
+
+        :param T: the Component class
+        """
         return self.component_arrs[self.components[T.__name__]]
 
     def add_component(self, component, entity):
+        """
+        Associate an instance of a component with an entity.
+
+        :param component: a component
+        :param entity: the entity
+        """
         idx = self.components[component.__class__.__name__]
         self.component_arrs[idx].append(component, key=entity.e_id)
 
     def remove_component(self, T, entity):
+        """
+        Remove components of type T with an entity.
+
+        :param T: a Component
+        :param entity: the entity
+        """
         idx = self.components[T.__name__]
         self.component_arrs[idx].remove_key(entity.e_id)
 
     def remove_all(self, entity):
+        """
+        Remove all components associated with an entity
+
+        :param entity: the entity
+        """
         for arr in self.component_arrs:
             try:
                 arr.remove_key(entity.e_id)
