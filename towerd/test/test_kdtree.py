ad1a323021bd9442e22b7bf45728ae762839ef65
Edmund Do <edo@uvm.edu>
2020-04-20 18:04:34 -0400

    changed to use single entity instead of list

--- towerd/test/test_kdtree.py
+++ towerd/test/test_kdtree.py
@@ -10,13 +10,13 @@ class TestKDTree(unittest.TestCase):
         self.k2 = kdtree.create(dimensions=2)
         for x in range(10):
             for y in range(10):
-                self.k2.add(EntityPoint2D(x/10, y/10, entities=[x, y]))
+                self.k2.add(EntityPoint2D(x/10, y/10, entity=x))
 
     def test_search_point(self):
         eps = [node.data for node, _ in self.k2.search_knn((0.2, 0.2), 3)]
 
         entities = []
         for ep in eps:
-            entities.extend(ep.entities)
+            entities.append(ep.entity)
 
         self.assertEqual(set(entities), set([2, 3]))
