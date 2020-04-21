1726f2efe6521571c68582bd9422eaaf598ce693
Edmund Do <edo@uvm.edu>
2020-04-21 04:05:20 -0400

    check the component data of the target before attacks

--- towerd/system/AttackSystem.py
+++ towerd/system/AttackSystem.py
@@ -1,47 +1,58 @@
 import math
-import kdtree
 
 from ..System import System
 
 
 class AttackSystem(System):
-    def update(self, dt, state, attackComps, vitalComps, locComps):
+    def update(self, dt, state, attackComps, vitalComps, locComps, factionComps):
         for entity in self.entities:
-            tree = self.state['tree']
+            tree = state['tree']
             entityLocComp = locComps[entity.ID]
             entityAttackComp = attackComps[entity.ID]
+            entityFactionComp = factionComps[entity.ID]
 
-            # If the attacker doesn't already have a target, set the target to be the nearest entity
-            if(entityAttackComp.target == None):
-                # Find entity nearest to this entity
-                nearestEntity = tree.search_nn(entityLocComp.x, entityLocComp.y)
-                entityAttackComp.target = nearestEntity
+            def calcDist(x0, y0, x1, y1):
+                return math.sqrt(math.pow(x1 - x0, 2) + math.pow(y1 - y0, 2))
 
-            targetAttackComp = attackComps[entityAttackComp.target.ID]
+            # If the attacker doesn't already have a target, set the target to
+            # be the nearest, in-range entity of the opposite faction
+            if not entityAttackComp.target:
+                ep2ds = tree.search_nn_dist((entityLocComp.x, entityLocComp.y), entityAttackComp.attackRange)
+                ep2ds = sorted(ep2ds, key=lambda t: calcDist(entityLocComp.x, entityLocComp.y, *t))
+
+                for ep2d in ep2ds:
+                    target = ep2d.entity
+                    targetFactionComp = factionComps[target.ID]
+                    targetAttackComp = attackComps[target.ID]
+
+                    if targetFactionComp != entityFactionComp and targetAttackComp.attackable:
+                        entityAttackComp.target = target
+                continue
+
+            # unset targets that are too far
             targetLocComp = locComps[entityAttackComp.target.ID]
+            distance = calcDist(entityLocComp.x, entityLocComp.y, targetLocComp.x, targetLocComp.y)
+            if distance > entityAttackComp.attackRange:
+                entityAttackComp.target = None
+                continue
+
             targetVitalComp = vitalComps[entityAttackComp.target.ID]
+            # calculate distance between entity and target
+            distance = math.sqrt(math.pow(targetLocComp.x - entityLocComp.x, 2) + math.pow(targetLocComp.y - entityLocComp.y, 2))
 
-            # if target is attackable
-            if(targetAttackComp.attackable == True):
-                # calculate distance between entity and target
-                distance = math.sqrt(math.pow(targetLocComp.x - entityLocComp.x, 2) + math.pow(targetLocComp.y - entityLocComp.y, 2))
+            # Calculate total damage based on entity's damage and the amount of time passed vs. attack speed
+            totalDamage = entityAttackComp.attackSpeed * dt * entityAttackComp.dmg
 
-                # Calculate total damage based on entity's damage and the amount of time passed vs. attack speed
-                totalDamage = entityAttackComp.attackSpeed * dt * entityAttackComp.dmg
+            # Do the damage to nearest entity
+            # if it has a shield, subtract from shield
+            if(targetVitalComp.shield > 0):
+                targetVitalComp.shield -= totalDamage
 
-                # if target is within range
-                if(distance <= entityLocComp.attackRange):
-                    # Do the damage to nearest entity
+                # If the shield is below 0, deal excess damage to health
+                if(targetVitalComp.shield <= 0):
+                    excessDamage = 0 - targetVitalComp.shield
+                    targetVitalComp.health -= excessDamage
 
-                    # if it has a shield, subtract from shield
-                    if(targetVitalComp.shield > 0):
-                        targetVitalComp.shield -= totalDamage
-
-                        # If the shield is below 0, deal excess damage to health
-                        if(targetVitalComp.shield <= 0):
-                            excessDamage = 0 - targetVitalComp.shield
-                            targetVitalComp.health -= excessDamage
-
-                    else:
-                        # target has no shield, deal damage directly to health
-                        targetVitalComp.health -= totalDamage
+            else:
+                # target has no shield, deal damage directly to health
+                targetVitalComp.health -= totalDamage
