877f1da296a2dab4c81855d846755e0496710b37
Carlos Castrejon <carlitoscs99@gmail.com>
2020-04-22 12:07:47 +0200

    New Game Class

--- towerd/Game.py
+++ towerd/Game.py
@@ -1,97 +1,59 @@
 import os
 import pygame
 import time
+import random
+
+
+pygame.mixer.music.load("xxx.mp3") #To play music while the game is running (still have to decided the track that we are going to play)
+
+class ECSManager:
+    def update(self, dt, state):
+        time.sleep(random.random()) #update all systems
+
+
+    def handle_input():
+        pass
+
+
+    def draw():
+        time.sleep(random.random())
 
 
 
-#music for the game (still have to decide which audio we are gonna use)
-pygame.mixer.music.load("xxx.mp3")
+#Game Logic
 
-class Game:
-	def __init__(self):
-		self.width = 1200 #Width of the pygame window
-		self.height = 700 #Height of the pygame window
-		self.win = pygame.display.set_mode((self.width, self.height))
-		self.mobs = []
-		self.towers = []
-		self.player_lives = 20
-		self.player_money = 1000
-		self.background = pygame.image.load(os.path.join("xxxxxxx", "xxxxx.png"))
-		self.background = pygame.transform.scale(self.background, (self.width, self.height))
-		self.wave = 0 #The starter wave
-		self.current_wave = waves[self.wave][:]
-		self.timer = time.time()
+quit = False
+quit_time = time.time() + 10
 
 
-	def build_tower():
-
-		x, y = pygame.mouse.get_pos()
-		name_list = ["archer", "mage", "soldier"]
-		#self.win.blit() ###With this we draw the towers in the pygame window
+dt = 0
+ecsm = ECSManager()
 
 
-	def generate_wave(self):
-
-		if sum(self.current_wave) == 0:
-			if len(self.mobs) == 0:
-				self.wave += 1
-				self.current_wave = waves[self.wave]
-		else:
-			wave_enemies = [Mobs()]
-
-            for x in range(len(self.current_wave)):
-				if self.current_wave[x] != 0:
-					self.enemys.append(wave_enemies[x])
-					self.current_wave[x] = self.current_wave[x] - 1
-					break
-
-    def gen_enemies(self): #This method handles all the waves generation
-
-		if(self.current_wave = 0):
-			if(len(self.enemies)) == 0:
-				self.wave += 1 #We add a new wave
-				self.current_wave = waves[self.wave]
+#create state below(maybe better as a class)
+state = {} #track entities, kdtree, etc
+state['entities'] = []
+state['map'] = None #Create map here
 
 
+while not quit:
+    start = time.time()
+    #use ECSManager to handle user input (create towers, etc)
+    handle_input()
+    #do updates, iterate over systems
+    ecsm.update(dt, state)
+    #do drawing, draw the entities/map/etc
+    draw()
 
-	def run(self): #This is the run method to control all the dynamics of the game
-		pygame.mixer.music.play(loops = -1)#The music will play undefinitely because the loop = -1
-		run = True
-		clock = pygame.time.Clock() #To set the FPS of the game
-		while run:
-			clock.tick(60) #This runs the fame at 60 FPS	
-
-
-			# main event loop
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					run = False
-
-				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_Q:
-						pos = pygame.mouse.get_pos()
-						if pos == :#i do not know how to check if the coords are valid for placing a tower
-							#draw archer tower
-					if event.key == pygame.K_W:
-						pos = pygame.mouse.get_pos()
-						if pos == :#same as before
-							#draw mage tower
-					if event.key == pygame.K_E:
-						pos = pygame.mouse.get_pos()
-						if pos == :#same as before
-							#draw melee tower
-
-					else:
-						
+    stop = time.time()
+    dt = stop - start  #this would be the clock.tick
+    print('fps', 1/dt)
 
 
-			#in case the player loses
-			if self.lives <= 0:
-				run = False
+    #handle quit
+    if time.time() > quit_time:
+    	quit = True
 
-
-
-
-
+    
 
 
