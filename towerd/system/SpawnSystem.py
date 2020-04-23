34153c6f209bea9944ed42de51a70d6ab7709efa
Edmund Do <edo@uvm.edu>
2020-04-23 19:14:48 -0400

    fixes to tests

--- towerd/system/SpawnSystem.py
+++ towerd/system/SpawnSystem.py
@@ -1,5 +1,6 @@
 import random
 
+from towerd.Map import PathType
 from towerd.System import System
 from towerd.component.Attack import Attack
 from towerd.component.Movement import Movement
@@ -11,13 +12,13 @@ from towerd.component.Vital import Vital
 class SpawnSystem(System):
     def update(self, dt, state, ecs_manager):
         # Get all path_start MapNodes and add them to a list
-        pathStartCoords = []
-        for mapNode in state.map.nodes:
-            if mapNode.pathType == "path_start":
-                pathStartCoords.append(mapNode)
+        pathStartNodes = []
+        for nodeID, mapNode in state['map'].nodes.items():
+            if mapNode.pathType == PathType.PATH_START:
+                pathStartNodes.append(mapNode)
 
         # Get the number of mobs based on what wave it is
-        waveNum = state.wave
+        waveNum = state['wave']
         numMobs = 0
         if waveNum == 1:
             numMobs = 5
@@ -31,10 +32,15 @@ class SpawnSystem(System):
             numMobs = 25
 
         for i in range(numMobs):
-            mob = ecs_manager.createEntity()
+            try:
+                mob = ecs_manager.createEntity()
+            except IndexError:
+                break
             # Get x and y coords from a random path_start
-            pathStartNode = random.choice(pathStartCoords)
-            pathTargetNode = random.choice(state.map[pathStartNode])
+            pathStartNode = random.choice(pathStartNodes)
+            pathTargetID = random.choice(state['map'].map[pathStartNode.id])
+            pathTargetNode = state['map'].nodes[pathTargetID]
+
             pathStartX = pathStartNode.x
             pathStartY = pathStartNode.y
 
@@ -48,4 +54,4 @@ class SpawnSystem(System):
             ecs_manager.addEntityComponent(mob, Attack(0.01, 2, 5, None, True))
             ecs_manager.addEntityComponent(mob, Faction(0))
 
-            # state.entites.update({mob : 0})
+            state['entities'][mob.ID] = mob
