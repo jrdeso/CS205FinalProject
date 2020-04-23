38cd4beaca61004f44b83036d16bf27c12196e81
Jordan D <54602007+jrdeso@users.noreply.github.com>
2020-04-23 18:28:18 -0400

    overwritten pygame.sprite

--- towerd/component/EntitySprite.py
+++ towerd/component/EntitySprite.py
@@ -1,7 +1,8 @@
 import dataclasses
-
+import pygame
 
 @dataclasses.dataclass
+@pygame.sprite.Sprite
 class EntitySprite:
     x: int
     y: int
--- towerd/system/SpriteSystem.py
+++ towerd/system/SpriteSystem.py
@@ -1,11 +1,20 @@
 
-
 from towerd.System import System
 from towerd.component.Attack import Attack
+from towerd.component.Movement import Movement
 from towerd.component.Faction import Faction
 from towerd.component.LocationCartesian import LocationCartesian
 from towerd.component.Vital import Vital
 
+from towerd.component.EntitySprite import EntitySprite
+
 
 class SpriteSystem(System):
     def update(self, dt, state, ecs_manager):
+        locComps = ecs_manager.getComponentArr(LocationCartesian)
+        spriteComps = ecs_manager.getComponentArr(EntitySprite)
+
+
+        # update locations for each entity
+        for entity in self.entities:
+            entityLocComp = locComps[entity.ID]
