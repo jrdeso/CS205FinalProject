8bbc0e97a2f34e322408c91f68494adfe8d6c7b9
Jordan D <54602007+jrdeso@users.noreply.github.com>
2020-04-25 19:54:54 -0400

    gui menu

--- towerd/ui/MainMenu.py
+++ towerd/ui/MainMenu.py
@@ -1,3 +1,4 @@
+import pygame
 import pygame_gui
 
 from towerd.ui.UI import UI
@@ -5,9 +6,27 @@ from towerd.ui.UI import UI
 
 class MainMenu(UI):
     def __init__(self, resolution):
+        # init pygame
+        pygame.init()
+        pygame.display.set_caption("Tower Defense")
+        font = pygame.font.Font('freesansbold.ttf', 72)
+        surface = pygame.display.set_mode(resolution)
+        background = pygame.Surface(resolution)
+        background.fill(pygame.Color((192, 192, 192)))
+
+        WELCOME = font.render("Tower Defense", True, (252, 252, 252))  # color - white
+
+        surface.blit(WELCOME, (int(resolution[0] / 5), int(resolution[1] / 4)))  # current hardcoded coordinates
+
         super().__init__(resolution)
-        # TODO: setup ui elements
+
+        #while True:
+        #    pygame.display.update()
+
 
     def handleEvents(self, event):
         # TODO: handle events associated with a ui element
         pass
+
+
+m = MainMenu((800, 600))
