68eba649750be01caac7e948482890d2e488bc09
Edmund Do <edo@uvm.edu>
2020-04-26 01:32:55 -0400

    integrate main menu into the game

--- towerd/ui/MainMenu.py
+++ towerd/ui/MainMenu.py
@@ -6,24 +6,29 @@ from towerd.ui.UI import UI
 
 class MainMenu(UI):
     def __init__(self, resolution):
-        # init pygame
-        pygame.init()
-        pygame.display.set_caption("Tower Defense")
-
-        surface = pygame.display.set_mode(resolution)
+        super().__init__(resolution)
         background = pygame.Surface(resolution)
         background.fill(pygame.Color((100, 100, 100)))
 
+        font = pygame.font.Font("freesansbold.ttf", 72)
+        welcomeMessage = font.render(
+            "Tower Defense", True, (252, 252, 252)
+        )
 
-        super().__init__(resolution, surface, background)
+        # make button to start game - quit
+        self.startButton = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((350, 0), (100, 50)),
+            text="Start",
+            manager=self.manager,
+        )
+        self.blits = [
+            (background, (0, 0)),
+            (welcomeMessage, (int(resolution[0] / 5), int(resolution[1] / 4))),
+        ]
 
-        #while True:
-        #    pygame.display.update()
-
-
-    def handleEvents(self, event):
-        # TODO: handle events associated with a ui element
-        pass
-
-
-m = MainMenu((800, 600))
+    def handleEvent(self, event):
+        from towerd.Game import GameEvent, R_PATHS
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == self.startButton:
+                    return (GameEvent.START, R_PATHS.map.default)
